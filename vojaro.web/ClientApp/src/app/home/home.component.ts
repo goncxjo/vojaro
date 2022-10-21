e23898669f111e9a00271988e9f0b8e0babc40fd
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2022-10-21 00:21:44 -0300

    se agrega boton carreras en home

--- vojaro.web/ClientApp/src/app/home/home.component.ts
+++ vojaro.web/ClientApp/src/app/home/home.component.ts
@@ -9,6 +9,7 @@ export class HomeComponent implements OnInit {
 
   MODULES = [
     { route: "universidades", name: "Universidades", icon: "fa fa-file-alt fa-4x", disabled: false },
+    { route: "carreras", name: "Carreras", icon: "fa fa-graduation-cap fa-4x", disabled: false },
     { route: "admin", name: "Administración", icon: "fa fa-cog fa-4x", disabled: true },
     { route: "security", name: "Seguridad", icon: "fa fa-user-shield fa-4x", disabled: true  },
   ]
