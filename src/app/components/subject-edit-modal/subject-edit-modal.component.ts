6631a80346b06e368411ff974aac80aa542f86e8
goncxjo <gonzalo.moreira@live.com.ar>
2025-03-11 18:50:29 -0300

    feat form edit subject

--- src/app/components/subject-edit-modal/subject-edit-modal.component.ts
+++ src/app/components/subject-edit-modal/subject-edit-modal.component.ts
@@ -1,11 +1,85 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { SubjectService } from '../../backend/services/subject.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { Subject } from '../../backend/models/subject/subject';
+import { UniversitySelectComponent } from '../university-select/university-select.component';
+import { CareerSelectComponent } from '../career-select/career-select.component';
 
 @Component({
   selector: 'app-subject-edit-modal',
-  imports: [],
+  imports: [ToastrModule, ReactiveFormsModule, UniversitySelectComponent, CareerSelectComponent],
   templateUrl: './subject-edit-modal.component.html',
   styleUrl: './subject-edit-modal.component.scss'
 })
 export class SubjectEditModalComponent {
+  form!: FormGroup;
+  subject!: Subject;
 
+  constructor(
+    private service: SubjectService,
+    private formBuilder: FormBuilder,
+    public activeModal: NgbActiveModal,
+    private toastr: ToastrService
+
+  ) { }
+
+  private buildForm(): FormGroup {
+    return this.formBuilder.group({
+      id: '',
+      name: [{ value: '' }, Validators.required],
+      year: [{ value: 1 }, Validators.required],
+      quarter: [{ value: 1 }, Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.form = this.buildForm();
+  }
+
+  ngAfterViewInit() {
+    this.form.controls['universityId'].addValidators(Validators.required); 
+    this.form.controls['careerId'].addValidators(Validators.required);
+    this.form.patchValue(this.subject)
+  }
+
+  save() {
+    const onSuccess = () => {
+      this.toastr.success('El registro fue guardado.', 'Guardar');
+      this.activeModal.close(this.form.getRawValue())
+    }
+  
+    const onError = (err: Error) => {
+      console.log(err);
+      this.toastr.error('Ha ocurrido un error', 'Error');
+    }
+
+    if (this.form.invalid) {
+      this.toastr.error('Por favor, completa los campos requeridos.', 'Error');
+      return;
+    }
+    debugger;
+    try {
+      const form = this.form.getRawValue();
+      const entity: Subject = {
+        id: form.id,
+        name: form.name,
+        universityId: form.universityId,
+        careerId: form.careerId,
+        year: form.year,
+        quarter: form.quarter,
+        mustApproved: form.mustApproved,
+        mustRegularize: form.mustRegularize 
+      };
+
+      if (entity.id) {
+        this.service.update(entity).then(onSuccess, onError);
+      } else {
+        return;
+      }
+    } catch (error) {
+      this.toastr.error('Ha ocurrido un problema', 'Error');
+    }
+  }
 }
