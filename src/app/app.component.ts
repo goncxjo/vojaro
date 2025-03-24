c75eb40ca70f4b34cb69a39818a339551465c7bc
goncxjo <gonzalo.moreira@live.com.ar>
2025-03-24 12:31:47 -0300

    feat implementation tailwindcss+daysiui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModal, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from './core/services/user.service';
-import { LoadingScreenComponent } from './core/loading-screen/loading-screen.component';
+import { LoadingScreenComponent } from './layout/loading-screen/loading-screen.component';
 import { WelcomeModalComponent } from './components/modals/welcome-modal/welcome-modal.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgbPopoverModule, LoadingScreenComponent, MatToolbarModule],
+  imports: [RouterOutlet, NgbPopoverModule, LoadingScreenComponent, MatToolbarModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
